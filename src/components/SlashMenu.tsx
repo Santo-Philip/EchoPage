32fa3950fc2acb81d5b879d4c823d4e3454e3fc3
Santo-Philip <santop7510@gmail.com>
2025-08-16 14:23:18 +0530

    small changes

--- src/components/SlashMenu.tsx
+++ src/components/SlashMenu.tsx
@@ -19,21 +19,273 @@ export const SlashMenu: React.FC<SlashMenuProps> = ({
   if (!show) return null;
   const items = [
     {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-heading1-icon lucide-heading-1"
+        >
+          <path d="M4 12h8" />
+          <path d="M4 18V6" />
+          <path d="M12 18V6" />
+          <path d="m17 12 3-2v8" />
+        </svg>
+      ),
       title: "Heading 1",
       onSelect: () => {
         editor?.chain().focus().setHeading({ level: 1 }).run();
       },
     },
     {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-heading2-icon lucide-heading-2"
+        >
+          <path d="M4 12h8" />
+          <path d="M4 18V6" />
+          <path d="M12 18V6" />
+          <path d="M21 18h-4c0-4 4-3 4-6 0-1.5-2-2.5-4-1" />
+        </svg>
+      ),
+      title: "Heading 2",
+      onSelect: () => {
+        editor?.chain().focus().setHeading({ level: 2 }).run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-heading3-icon lucide-heading-3"
+        >
+          <path d="M4 12h8" />
+          <path d="M4 18V6" />
+          <path d="M12 18V6" />
+          <path d="M17.5 10.5c1.7-1 3.5 0 3.5 1.5a2 2 0 0 1-2 2" />
+          <path d="M17 17.5c2 1.5 4 .3 4-1.5a2 2 0 0 0-2-2" />
+        </svg>
+      ),
+      title: "Heading 3",
+      onSelect: () => {
+        editor?.chain().focus().setHeading({ level: 3 }).run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-type-icon lucide-type"
+        >
+          <path d="M12 4v16" />
+          <path d="M4 7V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2" />
+          <path d="M9 20h6" />
+        </svg>
+      ),
+      title: "Text",
+      onSelect: () => {
+        editor?.chain().focus().setParagraph().run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-align-center-icon lucide-align-center"
+        >
+          <path d="M17 12H7" />
+          <path d="M19 18H5" />
+          <path d="M21 6H3" />
+        </svg>
+      ),
+      title: "Text Center",
+      onSelect: () => {
+        editor?.chain().focus().setTextAlign("center").run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-align-left-icon lucide-align-left"
+        >
+          <path d="M15 12H3" />
+          <path d="M17 18H3" />
+          <path d="M21 6H3" />
+        </svg>
+      ),
+      title: "Text Left",
+      onSelect: () => {
+        editor?.chain().focus().setTextAlign("left").run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-align-right-icon lucide-align-right"
+        >
+          <path d="M21 12H9" />
+          <path d="M21 18H7" />
+          <path d="M21 6H3" />
+        </svg>
+      ),
+      title: "Text Right",
+      onSelect: () => {
+        editor?.chain().focus().setTextAlign("right").run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-quote-icon lucide-quote"
+        >
+          <path d="M16 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z" />
+          <path d="M5 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z" />
+        </svg>
+      ),
+      title: "Quote",
+      onSelect: () => {
+        editor?.chain().focus().setBlockquote().run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-code-icon lucide-code"
+        >
+          <path d="m16 18 6-6-6-6" />
+          <path d="m8 6-6 6 6 6" />
+        </svg>
+      ),
+      title: "Code",
+      onSelect: () => {
+        editor?.chain().focus().setCodeBlock().run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-list-icon lucide-list"
+        >
+          <path d="M3 12h.01" />
+          <path d="M3 18h.01" />
+          <path d="M3 6h.01" />
+          <path d="M8 12h13" />
+          <path d="M8 18h13" />
+          <path d="M8 6h13" />
+        </svg>
+      ),
+      title: "List",
+      onSelect: () => {
+        editor?.chain().focus().toggleBulletList().run();
+      },
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-bold-icon lucide-bold"
+        >
+          <path d="M6 12h9a4 4 0 0 1 0 8H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h7a4 4 0 0 1 0 8" />
+        </svg>
+      ),
       title: "Bold",
       onSelect: () => {
         editor?.chain().focus().toggleBold().run();
       },
     },
   ];
-  const handleClick = () => {
-    editor?.chain().deleteRange(range).focus().run();
-  };
   return (
     <Tippy
       visible={show}
@@ -44,18 +296,35 @@ export const SlashMenu: React.FC<SlashMenuProps> = ({
         new DOMRect(coords.left, coords.bottom, 0, 0)
       }
       render={() => (
-        <div className="bg-white border border-gray-300 rounded shadow-lg w-48 max-h-64 overflow-y-auto z-50">
+        <div className="bg-text-secondary border border-text-muted text-bg-primary rounded  shadow-2xl w-48 max-h-64 overflow-y-auto z-50">
+          <button className="flex gap-2 justify-center bg-bg-primary text-text-primary items-center p-2 hover:bg-bg-primary/70 w-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="none"
+              viewBox="0 0 16 16"
+            >
+              <path stroke="currentColor" d="M8 1.5H3.5a1 1 0 0 0-1 1v11" />
+              <path stroke="currentColor" d="M14 14.5H3.5a1 1 0 1 1 0-2h10V8" />
+              <path
+                fill="currentColor"
+                d="m11.5.5.99 2.51L15 4l-2.51.99-.99 2.51-.99-2.51L8 4l2.51-.99L11.5.5ZM7.5 5l.707 1.793L10 7.5l-1.793.707L7.5 10l-.707-1.793L5 7.5l1.793-.707L7.5 5Z"
+              />
+            </svg>
+            <p>Auto Complete</p>
+          </button>
           {items.map((item, i) => (
             <button
               key={i}
-              onMouseDown={(e) => e.preventDefault()} 
+              onMouseDown={(e) => e.preventDefault()}
               onClick={() => {
                 editor?.chain().deleteRange(range).focus().run();
                 item.onSelect();
               }}
-              className="w-full text-left px-2 py-1 hover:bg-gray-100 cursor-pointer"
+              className="w-full flex gap-2 text-left px-2 py-1 hover:bg-text-primary/40 cursor-pointer"
             >
-              {item.title}
+              {item.icon} {item.title}
             </button>
           ))}
         </div>
