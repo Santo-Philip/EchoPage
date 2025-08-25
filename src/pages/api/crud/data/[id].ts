95d4a0403ed02f5a31ee9c69d822537227a00c23
Santo-Philip <santop7510@gmail.com>
2025-08-25 07:57:36 +0530

    updated security

--- src/pages/api/crud/data/[id].ts
+++ src/pages/api/crud/data/[id].ts
@@ -4,16 +4,53 @@ import type { APIRoute } from "astro";
 
 export const PATCH: APIRoute = async ({ request, params }) => {
   const blogId = params.id;
+  const email: string[] = import.meta.env.EMAIL.split(",");
+
+  const user = await supabase.auth.getUser();
+  if (!user.data.user) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+    });
+  }
+  // Allow only users from env according to email
+  // if (!email.includes(user.data.user.email)) {
+  //   return new Response(JSON.stringify({ error: "Not Allowed" }), { status: 401 })
+  // }
+  const { data: existing, error: existingDataerror } = await supabase
+    .from("draft")
+    .select("*")
+    .eq("id", blogId);
+
+  if (existingDataerror) {
+    return new Response(JSON.stringify({ error: existingDataerror.message }), {
+      status: 500,
+    });
+  }
+
+  if (existing.length === 0) {
+    return new Response(JSON.stringify({ error: "Blog not found" }), {
+      status: 404,
+    });
+  }
+  if (existing[0].author !== user.data.user.id) {
+    return new Response(JSON.stringify({ error: "Not Allowed" }), {
+      status: 401,
+    });
+  }
 
   if (!blogId) {
-    return new Response(JSON.stringify({ error: "Blog ID missing" }), { status: 400 });
+    return new Response(JSON.stringify({ error: "Blog ID missing" }), {
+      status: 400,
+    });
   }
 
   let data: DraftContent;
   try {
     data = await request.json();
   } catch {
-    return new Response(JSON.stringify({ error: "Invalid JSON" }), { status: 400 });
+    return new Response(JSON.stringify({ error: "Invalid JSON" }), {
+      status: 400,
+    });
   }
 
   try {
@@ -22,33 +59,20 @@ export const PATCH: APIRoute = async ({ request, params }) => {
       .update(data)
       .eq("id", blogId);
     if (error) {
-      return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+      return new Response(JSON.stringify({ error: error.message }), {
+        status: 500,
+      });
     }
 
-    return new Response(JSON.stringify({ message: "Draft updated successfully" }), {
-      status: 200,
-    });
+    return new Response(
+      JSON.stringify({ message: "Draft updated successfully" }),
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
-    return new Response(JSON.stringify({ error: "Server error" }), { status: 500 });
+    return new Response(JSON.stringify({ error: "Server error" }), {
+      status: 500,
+    });
   }
 };
-
-export const DELETE: APIRoute = async ({ params }) => {
-  const blogId = params.id; 
-  if (!blogId) {
-    return new Response(JSON.stringify({ error: "Blog ID missing" }), { status: 400 });
-  }
-
-  try {
-    const { error } = await supabase.from("draft").delete().eq("id", blogId )
-    if (error) {
-      return new Response(JSON.stringify({ error: error.message }), { status: 500 })
-    
-    }
-    return new Response(JSON.stringify({ message: "Draft deleted successfully" }), {
-      status: 200,
-    })
-  } catch {
-    return new Response(JSON.stringify({ error: "Server error" }), { status: 500 })
-  }
-}
