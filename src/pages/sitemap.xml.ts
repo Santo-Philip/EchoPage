9528ea22ef5a1a7faecada082f209d811d193556
Santo-Philip <santop7510@gmail.com>
2025-09-11 12:07:48 +0530

    auto sitemap added

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,25 @@
+import { supabase } from "@/lib/supabase";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+  const { data: posts } = await supabase.from('draft').select('slug');
+  const site = import.meta.env.SITE 
+    
+  const urls = posts?.map(
+      (p) =>
+        `<url><loc>${site}/blog/${p.slug}</loc></url>`
+    )
+    .join("");
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+    </urlset>`,
+    {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    }
+  );
+};
