b8e6874ae2420feebe601983dda5aea9c5743260
Santo-Philip <santop7510@gmail.com>
2025-09-15 00:39:58 +0530

    fixed auth error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import type { MiddlewareHandler } from "astro";
 import { createHmac } from "node:crypto";
+import { createServerClient } from '@supabase/ssr'
+
 
 const SECRET = import.meta.env.ANON_SECRET || "e725cc11bbcf0e94f130a6f86c3346fc97f470be1a014bd943d64fa24be73e9c";
+
 
 function signAnonId(id: string): string {
   const hmac = createHmac("sha256", SECRET);
@@ -25,6 +28,25 @@ function verifyToken(token: string): boolean {
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
   let anonToken = context.cookies.get("anon_id")?.value;
+
+   const supabase = createServerClient(
+  import.meta.env.SUPABASE_URL || process.env.SUPABASE_URL!,
+  import.meta.env.SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!,
+
+    {
+      cookies: {
+        get(name) {
+          return context.cookies.get(name)?.value
+        },
+        remove(name, options) {
+          context.cookies.delete(name, options)
+        },
+        set(name, value, options) {
+          context.cookies.set(name, value, options)
+        },
+      },
+    }
+  )
 
   if (!anonToken || !verifyToken(anonToken)) {
     anonToken = generateToken();
