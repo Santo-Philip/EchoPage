f6953e6faa28bfdfd5e4a33a83dc3e618ea38c56
Santo-Philip <santop7510@gmail.com>
2025-09-14 23:46:07 +0530

    fixed login issue

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -21,14 +21,14 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
     httpOnly: true,
     secure: true,
     sameSite: "lax",
-    maxAge: 60 * 60 * 24 * 7, // 7 days
+    maxAge: 60 * 60 * 24 * 7,
   });
   cookies.set("sb-refresh-token", refresh_token, {
     path: "/",
     httpOnly: true,
     secure: true,
     sameSite: "lax",
-    maxAge: 60 * 60 * 24 * 30, // 30 days
+    maxAge: 60 * 60 * 24 * 30,
   });
 
   return redirect("/");
